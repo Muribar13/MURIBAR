bdc93ef3953608695460c6e9c02bc4aa898ee4d4
Muribar <m.rizaldyakbar20@gmail.com>
2022-11-04 17:58:09 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily:{
+      'sans' :['Lexend Zetta', "sans-serif"],
+      'open-sans' :['Open Sans', "sans-serif"]
+    },
+    extend: {
+      colors : {ayas:"#11253A"}
+    },
+  },
+  plugins: [],
+}
